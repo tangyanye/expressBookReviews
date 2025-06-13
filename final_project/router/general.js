4264b36698335769a3f643fadd2f3f14e8c1ed8c
tangyanye <tangyanye@gmail.com>
2025-06-13 17:01:26 -0400

    Task3 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,8 +28,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const matchingBooks = [];
+    
+    for (const isbn in books) {
+        if (books[isbn].author === author) {
+            matchingBooks.push({ isbn, ...books[isbn] });
+        }
+    }
+
+    if (matchingBooks.length > 0) {
+        res.send(matchingBooks);
+    } else {
+        res.status(404).json({ message: "No books found for this author" });
+    }
 });
 
 // Get all books based on title
