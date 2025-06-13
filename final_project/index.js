30cccc020ebc640c21b56338969d307b8f0f964b
tangyanye <tangyanye@gmail.com>
2025-06-13 15:57:47 -0400

    Task1 finished

--- final_project/index.js
+++ final_project/index.js
@@ -35,4 +35,4 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running on port ${PORT}"));
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
