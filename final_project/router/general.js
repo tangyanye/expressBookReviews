b04b3cb0cfc6097c68b4d44d3e9996bb7f1b16ab
tangyanye <tangyanye@gmail.com>
2025-06-13 21:32:59 -0400

    Task7 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,7 +14,6 @@ public_users.post("/register", (req,res) => {
             return res.status(409).json({ message: "Username already exists." });
         }
     }
-
     users.push({ username, password });
     return res.status(201).json({ message: "User registered successfully." });
 });
