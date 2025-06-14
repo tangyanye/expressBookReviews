b4167e886dcb09034916c861ed6696b5344c8fd4
tangyanye <tangyanye@gmail.com>
2025-06-14 10:10:40 -0400

    Task8 completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -45,10 +45,25 @@ regd_users.post("/login", (req,res) => {
 });
 
 // Add a book review
+
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session.authorization?.username;
+
+    if (!username) {
+        return res.status(401).json({ message: "User not logged in" });
+    }
+
+    if (books[isbn]) {
+        if (!books[isbn].reviews) books[isbn].reviews = {};
+        books[isbn].reviews[username] = review;
+        return res.status(200).json({ message: "Review added/updated successfully" });
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
